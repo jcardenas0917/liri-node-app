3e8d3f3f626c28bbd9b7b3d6077e060ee15287fa
jaime cardenas <jcardenas@fixcityrepairs.com>
2019-08-08 21:40:17 -0400

    working on spotify API

--- liri.js
+++ liri.js
@@ -1,20 +1,13 @@
 require("dotenv").config();
 var axios = require("axios");
 var moment = require('moment');
-// var Spotify = require('node-spotify-api');
 moment().format();
-// var keys = require("./keys.js");
-// var spotify = new Spotify(keys.spotify)
-// const spotifyID =spotify.credentials.id;
-// const spotifySecret = spotify.credentials.secret;
-
 
 //--------------------------------------------------------------
 var command = process.argv[2];
 var input = process.argv[3];
 var movieQueryUrl = "";
 var concertQueryUrl = "";
-var spotifyQueryUrl = "";
 var displayMovie = result => {
     console.log(result.Title);
     console.log(result.Year);
@@ -35,32 +28,32 @@ var displayConcert = result => {
         console.log(result[i].venue.country);
         console.log(result[i].venue.region);
         console.log(result[i].venue.city);
-        console.log(moment(result[i].datetime).format("MM/DD/YYYY"));
+        console.log(moment(result[i].venue.datetime).format("MM/DD/YYYY"));
         console.log("______________________")
     }
 
 }
 
 var displaySong = results => {
-    console.log(results);
+    for (var i = 0; i < results.length; i++) {
+        
+        console.log(results[i].artists[0].name);
+        console.log(results[i].name);
+        console.log(results[i].uri);
+       
+        
+        console.log("______________________")
+    }
 }
 var apiCall = queryUrl => {
     axios.get(queryUrl).then(
         function (response) {
-            
-            switch(command){
-                case "movie-this":
+            if (command==="movie-this"){
                 displayMovie(response.data);
-                break;
-                case "concert-this":
+            }else if(command==="concert-this"){
                 displayConcert(response.data);
-                break;
-                case "spotify-this-song":
-                displaySong(response);
-                break;
-                case "do-what-it-says":
-                break;
             }
+           
         })
         .catch(function (error) {
             if (error.response) {
@@ -85,18 +78,18 @@ var apiCall = queryUrl => {
 
 }
 
-var spotifyCall = () =>{
+var spotifyCall = () => {
     var Spotify = require('node-spotify-api');
     var keys = require("./keys.js");
     var spotify = new Spotify(keys.spotify);
     spotify
-  .search({ type: 'track', query: input })
-  .then(function(response) {
-    console.log(response.tracks);
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+        .search({ type: 'track', query: input })
+        .then(function (response) {
+            displaySong(response.tracks.items);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 }
 
 
@@ -107,22 +100,46 @@ switch (command) {
         return;
 
     case "spotify-this-song":
-            // spotifyQueryUrl = "https://api.spotify.com/v1/search?q="+input+"&type=track&market=US&limit=10&offset=5";
-            spotifyCall();
+        spotifyCall();
         return;
 
     case "movie-this":
-        // if (input1==="Undefined"){
-        //     console.log(input1)
-        //     movieQueryUrl = "https://www.omdbapi.com/?t=MrNobody&apikey=2ccc910c";
-        //     console.log(movieQueryUrl)
-        //     apiCall(movieQueryUrl);
-        // }else{
         movieQueryUrl = "https://www.omdbapi.com/?t=" + input + "&apikey=2ccc910c";
         apiCall(movieQueryUrl);
         return;
-    // }
+    
     case "do-what-it-says":
+            var fs = require("fs");
+
+            // This block of code will read from the "random.txt" file.
+            // The code will store the contents of the reading inside the variable "data"
+            fs.readFile("random.txt", "utf8", function(error, data) {
+            
+              // If the code experiences any errors it will log the error to the console.
+              if (error) {
+                return console.log(error);
+              }
+            
+              // We will then print the contents of data
+              console.log(data);
+            
+              // Then split it by commas (to make it more readable)
+              var dataArr = data.split(",");
+            
+              // We will then re-display the content as an array for later use.
+
+              if (dataArr[0]==="spotify-this-song"){
+                  input = dataArr[1];
+                  spotifyCall()
+              }
+            //   else if(dataArr[0]==="concert-this"){
+            //       input = dataArr[1];
+            //       concertQueryUrl = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp";
+            //       apiCall(concertQueryUrl);
+
+            //   }
+            });
+            
         return;
 
     default:
